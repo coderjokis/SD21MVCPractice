e016abb7ea02692d7db6f5815bbfd76b12c09ff5
coderjokis <joseph.maglalang@robertsoncollege.net>
2015-12-04 11:23:08 -0600

    Added Book model

---
+++ SD21MVCPractice/SD21MVCPractice/Models/BookModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SD21MVCPractice.Models
+{
+    public class BookModel
+    {
+        public string ID { get; set; }
+        public string BookTitle { get; set; }
+        public string  Description { get; set; }
+
+    }
+}
